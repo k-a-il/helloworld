46379057d891183513a44b3573b680dd30ca6d39
k-a-il <61540676+k-a-il@users.noreply.github.com>
2021-02-03 16:11:03 +0100

    Update ReadAndPrintScores.java

--- src/main/java/ReadAndPrintScores.java
+++ src/main/java/ReadAndPrintScores.java
@@ -7,9 +7,7 @@ public class ReadAndPrintScores
     public static void main(String[] args)
     {   try
         {   Scanner s = new Scanner( new File("scores.dat") );
-            while( s.hasNextInt() )
-            {   System.out.println( s.nextInt() );
-            }
+           
         }
         catch(IOException e)
         {       System.out.println( e );
