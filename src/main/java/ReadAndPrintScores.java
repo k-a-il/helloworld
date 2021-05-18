7e96e97458743529f16d0335e89cd2669108f32e
k-a-il <61540676+k-a-il@users.noreply.github.com>
2021-05-18 16:52:34 +0200

    Update ReadAndPrintScores.java

--- src/main/java/ReadAndPrintScores.java
+++ src/main/java/ReadAndPrintScores.java
@@ -7,8 +7,8 @@ public class ReadAndPrintScores
     public static void main(String[] args)
     {   try
         {
-         Scanner s2 = new Scanner( new File("scorывфывааes.dat") );
-           
+            Scanner s2 = new Scanner( new File("scorывфывааes.dat") );
+            Scanner s3 = new Scanner( new File("scorывфывааes.dat") );
         }
         catch(IOException e)
         {       System.out.println( e );
